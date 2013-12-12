9fa08eb6a02d019a79cd7871b75b34f660826f14
Cristi Dumitru <cdumitru@insynctive.com>
2013-12-12 15:27:45 -0800

    Added HTML documentation as Pages

---
+++ search/all_64.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['datasource',['Datasource',['../class__28_1_1io_1_1Project_1_1Model_1_1Datasource.html',1,'_28::io::Project::Model']]]
+];
---
+++ search/functions_67.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['getcollectionmetadata',['getCollectionMetadata',['../class__28_1_1io_1_1Project_1_1Api_1_1__datasourcesApi.html#ab06ecd195e48fc618c5e05a144ef6488',1,'_28::io::Project::Api::_datasourcesApi']]],
+  ['getcollectionmetadataasstring',['getCollectionMetadataAsString',['../class__28_1_1io_1_1Project_1_1Api_1_1__datasourcesApi.html#a3a230e59eace82a2ba5f49ab39f1ce02',1,'_28::io::Project::Api::_datasourcesApi']]],
+  ['getdatasource',['getDatasource',['../class__28_1_1io_1_1Project_1_1Api_1_1__datasourcesApi.html#a3235b441a6dad8393302553ed745eed4',1,'_28::io::Project::Api::_datasourcesApi']]],
+  ['getdatasourceasstring',['getDatasourceAsString',['../class__28_1_1io_1_1Project_1_1Api_1_1__datasourcesApi.html#ab32dfb23f87630e72388355351bcfa89',1,'_28::io::Project::Api::_datasourcesApi']]],
+  ['getdatasourcecontents',['getDatasourceContents',['../class__28_1_1io_1_1Project_1_1Api_1_1__datasourcesApi.html#a2f35db5bb5e24b1d5ccfae2575e74f68',1,'_28::io::Project::Api::_datasourcesApi']]],
+  ['getdatasourcecontentsasstring',['getDatasourceContentsAsString',['../class__28_1_1io_1_1Project_1_1Api_1_1__datasourcesApi.html#aecc45e20465e735bcb5472c89da62b93',1,'_28::io::Project::Api::_datasourcesApi']]],
+  ['getitem',['getItem',['../class__28_1_1io_1_1Project_1_1Api_1_1__datasourcesApi.html#adfe17b2907007059fcda4cada91d2559',1,'_28::io::Project::Api::_datasourcesApi']]],
+  ['getitemasstring',['getItemAsString',['../class__28_1_1io_1_1Project_1_1Api_1_1__datasourcesApi.html#ae3fddc477da1a18c6e5d1a79384f1d98',1,'_28::io::Project::Api::_datasourcesApi']]],
+  ['getquery',['getQuery',['../class__28_1_1io_1_1Project_1_1Api_1_1__queriesApi.html#afdecaf52eea03198221fa8619198cea2',1,'_28::io::Project::Api::_queriesApi']]],
+  ['getqueryasstring',['getQueryAsString',['../class__28_1_1io_1_1Project_1_1Api_1_1__queriesApi.html#a030935acf64ff27d707da304605b26a6',1,'_28::io::Project::Api::_queriesApi']]],
+  ['getqueryplan',['getQueryPlan',['../class__28_1_1io_1_1Project_1_1Api_1_1__queriesApi.html#a16318c734478f0c3318731f9e2c00e5c',1,'_28::io::Project::Api::_queriesApi']]],
+  ['getqueryplanasstring',['getQueryPlanAsString',['../class__28_1_1io_1_1Project_1_1Api_1_1__queriesApi.html#a4cd5a60f27b797e21704feeed9f32046',1,'_28::io::Project::Api::_queriesApi']]]
+];
